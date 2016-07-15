47598a0cd97d5969af347371328eb101382ed8be
ruizhang331 <ruizhang331@gmail.com>
2016-07-14 22:50:58 -0700

    Simple pymongo example
    Simple pymongo example on how to use mongo DB to query sensor data

---
+++ pymongo_ex.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+from random import randint
+from datetime import datetime
+
+try:
+    client = MongoClient()
+    db = client.test_database
+    collection = db.test_collection
+    sensor_data = db.sensor_data
+    sensor_data.insert(dict(date=datetime.now(), value=randint(1, 100)))
+
+    now = datetime.now()
+    start = now.replace(hour=8, minute=0, second=0, microsecond=0)
+    end = datetime.strptime("14/07/16 16:30", "%d/%m/%y %H:%M")
+
+    for data_reading in sensor_data.find({"$and": [{'date':{"$gt":start,'$lt':now}},{"value": {"$lt": 50}}]}).sort("value"):
+        print data_reading
+
+
+except pymongo.errors.ConnectionFailure, e:
+   print "Could not connect to MongoDB: %s" % e 
+
