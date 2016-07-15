889d19658ef35485c9a9dc617b1c7d4a84fa5708
ruizhang331 <ruizhang331@gmail.com>
2016-07-14 22:52:01 -0700

    Update pymongo_ex.py

--- pymongo_ex.py
+++ pymongo_ex.py
@@ -7,8 +7,9 @@ try:
     db = client.test_database
     collection = db.test_collection
     sensor_data = db.sensor_data
+    #fake data generation
     sensor_data.insert(dict(date=datetime.now(), value=randint(1, 100)))
-
+    # query mongo DB with data between start and end time, and value smaller than certain threhold 
     now = datetime.now()
     start = now.replace(hour=8, minute=0, second=0, microsecond=0)
     end = datetime.strptime("14/07/16 16:30", "%d/%m/%y %H:%M")
